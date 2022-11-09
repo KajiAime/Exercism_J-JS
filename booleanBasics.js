40711ad34c73388c3429455b20fa8d405c0ba44c
Nde-Ebom <ndekaji@gmail.com>
2022-11-09 23:51:28 +0100

    I am illustrating the boolean type.
    I have more to add to this basic functions later.

---
+++ booleanBasics.js
@@ -0,0 +1,12 @@
+/*
+*In boolean algebra, we havea few operators
+*to take note of in JavaScript. These are:
+*==, !, !=, ||, &&, 
+*/
+function isNotEqual(a, b){
+  return a != b;
+}
+
+function isEqual(a, b){
+  return a == b;
+}
